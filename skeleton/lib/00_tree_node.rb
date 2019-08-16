a1a528f84de22e7109b334813614857c54f40f54
App Academy Student <aa@Apps-Mac-mini-69.local>
2019-08-16 13:36:30 -0400

    Initial commit

---
+++ skeleton/lib/00_tree_node.rb
@@ -0,0 +1,38 @@
+require "byebug"
+
+class PolyTreeNode
+  attr_accessor :value, :parent, :children
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end 
+
+  def parent=(node)
+    #debugger
+      old_parent = self.parent
+      if old_parent != nil
+        old_parent.children.delete(self)
+      end
+      @parent = node  
+      if self.parent != nil 
+      parent.children << self if !node.children.include?(self)
+      end      
+  end
+
+  def add_child(node)
+    node.parent=(self)
+  end
+
+  def remove_child(node)
+    if node.parent != nil 
+     node.parent = nil
+    else
+      raise "node is not a child"
+    end  
+  end
+
+end
+
+# node_test = PolyTreeNode.new(7)
+# node_test.parent=()
